05ae2a885d42d7731c5bde01c2027512da2a0cb6
Snoozeds <88577069+Snoozeds@users.noreply.github.com>
2025-02-21 12:32:54 +0000

    Add pwa stuffs

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,26 @@ export default defineConfig({
       includeAssets: ['**/*'],
       manifest: {
         name: 'BlahajsSpinningBlahajs',
+        short_name: 'Blahaj',
+        description: 'Spinning shork :3',
+        theme_color: '#ffffff',
+        background_color: '#577183',
         display: 'fullscreen',
         start_url: '/',
-      },
+        icons: [
+            {
+                src: 'pwa192.png',
+                sizes: '192x192',
+                type: 'image/png',
+            },
+            {
+                src: 'pwa512.png',
+                sizes: '512x512',
+                type: 'image/png',
+            },
+        ],
+    },
     }),
   ],
 });
+
